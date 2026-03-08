d3292d2c07da7424138264634b6bc3e296a2da3c
TulasiNND <116662776+tulasinnd@users.noreply.github.com>
2026-03-08 19:44:23 +0530

    Delete src directory

--- src/model/embeddings.py
+++
@@ -1,8 +0,0 @@
-class Embeddings(nn.Module):
-    def __init__(self, vocab_size, d_model):
-        super().__init__()
-        self.embedding= nn.Embedding(vocab_size,d_model)
-
-    def forward(self, x_batch):
-        x_emb=self.embedding(x_batch)
-        return x_emb
