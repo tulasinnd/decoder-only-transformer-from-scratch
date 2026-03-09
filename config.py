5bafcd050e30b5830f7871ecc9dabc19a43e2969
tulasinnd <tulasinnd@gmail.com>
2026-03-09 14:27:33 +0530

    Add project configuration and hyperparameters

---
+++ config.py
@@ -0,0 +1,10 @@
+
+d_model= 8
+seq_len= 10
+max_seq_len=64 # for flexible sequence length
+batch_size=2
+num_heads=4
+num_layers=3
+num_steps = 400        # training iterations
+print_every = 200       # logging frequency
+learning_rate = 3e-4 
