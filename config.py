cb1cf4b99a06fd83438edbaf74c66668e745c78e
tulasinnd <tulasinnd@gmail.com>
2026-03-10 21:14:38 +0530

    Refine decoder training configuration

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 
-d_model= 8
-seq_len= 10
-max_seq_len=64          # for flexible sequence length
-batch_size=2
+d_model= 128
 num_heads=4
-num_layers=3
-num_steps = 400         # training iterations
+num_layers=4
+
+seq_len= 64
+max_seq_len=128          # for flexible sequence length
+batch_size=16
+
+
+num_steps = 3000         # training iterations
 print_every = 200       # logging frequency
 learning_rate = 3e-4 
