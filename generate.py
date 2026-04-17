d129716160eb20bf73f91d37fc205d04de7f5e01
tulasinnd <tulasinnd@gmail.com>
2026-04-17 17:27:51 +0530

    chore: refactor file names

--- generate.py
+++ generate.py
@@ -1,7 +1,7 @@
 import torch
 from model.decoder import Decoder
 from data.dataset import tokenizer,get_vocab_size
-from config import *
+from default_config import *
 from generation.generation_utils import generate,get_input_ids,print_generation
 from utils.utils import set_seed,get_device
 set_seed(seed)
@@ -10,7 +10,7 @@ device = get_device()
 # load model from checkpoint
 vocab_size = get_vocab_size()
 model = Decoder(vocab_size, max_seq_len, d_model, num_heads, num_layers)
-ckpt = torch.load("checkpoints/resume_checkpoint.pt", map_location=device)
+ckpt = torch.load("checkpoints/best_model.pt", map_location=device)
 missing, unexpected = model.load_state_dict(ckpt["model_state"], strict=False)
 if missing: print("Missing:", missing)
 if unexpected: print("Unexpected:", unexpected)
